af73459b2fb6ff03313df4bbfd95ad2deb289f04
abalakka <anirudhbalakka@gmail.com>
2020-09-27 18:54:06 +0530

    Remove secrets.js

--- src/secrets.js
+++
@@ -1,3 +0,0 @@
-export const SLACK_OAUTH_TOKEN = 'xoxb-1385577957696-1396099578308-cPU1d96pmCk6dqdQIhah3ryY';
-export const BOT_TEST_CHANNEL = '#bot-test';
-export const CODE_CHANNEL = '#code-challenge';
