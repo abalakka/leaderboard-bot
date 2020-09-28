1c2d36ab1b4b79a6f8921fd11bc3aa4a0635d219
abalakka <anirudhbalakka@gmail.com>
2020-09-28 10:15:23 +0530

    Refactor code and change post leaderboard to code channel

--- src/app.js
+++ src/app.js
@@ -26,21 +26,25 @@ rtm.on('slack_event', async (eventType, event) => {
         } 
         else if(event.text === '!ranks') {
             respond(BOT_TEST_CHANNEL, `Leaderboard requested by  <@${event.user}>`);
-            let msg = 'Todays leaderboard:\nRank\t\tName\n';
-            getLeaderboard().then(resp => {
-                resp.forEach(user => {
-                    msg += user.rank + '\t\t' + user.name  + '\n';
-                });
-                respond(BOT_TEST_CHANNEL, msg);
-            }).catch(error => {
-                console.log(error);
-            });
+            postLeaderboard();
         }
         else if(event.text === '!announceQs') {
             respond(CODE_CHANNEL, 'New questions have just been added to the challenge! Happy solving :)');
         }
     }
 });
+
+async function postLeaderboard() {
+    let msg = 'Todays leaderboard:\nRank\t\tName\n';
+    getLeaderboard().then(resp => {
+        resp.forEach(user => {
+            msg += user.rank + '\t\t' + user.name + '\n';
+        });
+        respond(CODE_CHANNEL, msg);
+    }).catch(error => {
+        console.log(error);
+    });
+}
 
 async function respond(channel, text) {
     sendMessage(channel, text);
